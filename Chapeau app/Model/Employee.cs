f2b075ae52d7b5d9236cc981fa448a80e893164f
Sorint-original <85764494+Sorint-original@users.noreply.github.com>
2024-05-21 09:16:50 +0200

    Model classes start

---
+++ Chapeau app/Model/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public Role Role { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
+
+public enum Role
+{
+    waiter,chef,barman
+}
