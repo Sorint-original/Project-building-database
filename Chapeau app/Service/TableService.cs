f8e39a363213559f2a084c99b3bb2cbae12e5314
Adrianosa1 <144953774+Adrianosa1@users.noreply.github.com>
2024-06-03 11:45:14 +0300

    table

---
+++ Chapeau app/Service/TableService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class TableService
+    {
+
+        private TableDao tablekDao;
+
+        public TableService()
+        {
+            tablekDao = new TableDao();
+        }
+    }
+}
