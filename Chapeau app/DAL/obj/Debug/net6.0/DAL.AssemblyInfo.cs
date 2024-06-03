2d8bf2ffbe47857b6e65caf17bb16cd30e2dfac5
Sorint-original <85764494+Sorint-original@users.noreply.github.com>
2024-06-03 10:52:22 +0200

    Conflict fix

--- Chapeau app/DAL/obj/Debug/net6.0/DAL.AssemblyInfo.cs
+++ Chapeau app/DAL/obj/Debug/net6.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+145f7ffc3a2e36647cfcaf4ae513bf990248793e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+865738f01866d0c7c806b51b9fe2f2fca5f1f0b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
