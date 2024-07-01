a4686cc4ad131e1b2e40524350289147c29a9340
Sorint-original <85764494+Sorint-original@users.noreply.github.com>
2024-07-01 08:11:11 +0200

    some fixes

--- Chapeau app/Service/obj/Debug/net6.0/Service.AssemblyInfo.cs
+++ Chapeau app/Service/obj/Debug/net6.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91bf4f3c2a6d448110f4f555098e54b769d27f1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6836ac5278d74c59d23d031b57d50e1f3e6bcad4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
